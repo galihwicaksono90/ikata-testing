b6a5071c2ff7912463a948ad4fe9b0803a4932b0
Galih Wicaksono <galihwicaksono90@gmail.com>
2022-05-12 09:30:16 +0700

    Fix document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class _Document extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
