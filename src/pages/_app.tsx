233e0d516dfe2a45637292986c3ed8cb05680211
Galih Wicaksono <galihwicaksono90@gmail.com>
2022-06-20 10:04:03 +0700

    Fix font not loaded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,10 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { wrapper } from "redux/store";
 import { theme } from "theme";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
 
 function App(props: AppProps) {
   const { Component, pageProps } = props;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,18 +9,7 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="crossorigin"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,500&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
